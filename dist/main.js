ea8557540896080dda6cd9c51524d08b78f88ed4
Siiam Qyf <44990451+SiamQyf@users.noreply.github.com>
2024-02-04 00:31:54 +0600

    First push

--- dist/main.js
+++ dist/main.js
@@ -1,52 +1,9 @@
-let [secs, mins, hrs] = [0, 0, 0];
-let displayId = document.getElementById('displayX');
-let btnStart = document.getElementById('btnStart');
-let btnStop = document.getElementById('btnStop');
-let btnReset = document.getElementById('btnReset');
-let timer = null;
+'use strict'
+let colorbox = document.getElementById('colorbox');
+let btn  = document.getElementById('btn');
 
-function stopwatch(){
-    secs++; 
-    if (secs == 60){ 
-        secs = 0;  
-        mins++; 
-        if (mins == 60) {
-            mins = 0;  
-            hrs++; 
-        }
-    }
-    let secsMod = secs < 10 ? ('0' + secs) : secs;
-    let minsMod = mins < 10 ? ('0' + mins) : mins;
-    let hrsMods = hrs < 10 ? ('0' + hrs) : hrs;
-    displayId.innerHTML = hrsMods + ':' + minsMod +':' + secsMod;
-}
-
-btnStart.addEventListener('click', function(){
-    if (timer != null){
-        clearInterval(timer);
-    };   
-    timer = setInterval(stopwatch, 1000);
-    btnStart.innerHTML = "STARTED";
-    btnStop.innerHTML = "PAUSE";
-    btnStart.classList.add("focus:bg-slate-400");
- })
-
- btnStop.addEventListener('click', function(){
-    clearInterval(timer);
-    if(displayId.innerHTML != "00:00:00"){
-        btnStop.clicked = false
-        btnStart.innerHTML = "RESUME";
-        btnStop.innerHTML = "PAUSED";
-        btnStop.classList.add("focus:bg-slate-400");
-    }
-})
-
-btnReset.addEventListener('click', function(){
-    displayId.innerHTML = "00:00:00";
-    clearInterval(timer);
-    btnStart.innerHTML = "START";
-    btnStop.innerHTML = "PAUSE";
-    btnReset.classList.add("focus:bg-slate-400");
-    [secs, mins, hrs] = [0, 0, 0];
+btn.addEventListener('click', function(){
+    colorbox.style.backgroundColor = 
+    "#" + ((1 << 24) * Math.random() | 0).toString(16).padStart(6, "0")
 })
 
